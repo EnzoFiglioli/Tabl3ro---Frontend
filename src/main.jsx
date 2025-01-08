6002256891c60ecd71087b653659cc22b5db1b73
Enzo Figlioli <enzofiglioli.p@gmail.com>
2025-01-08 15:45:37 -0300

    exportar componente Register y actualizar importación en main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
-import Register from "./pages/register.jsx";
 import { ModalProvider } from "./context/ModalContext";
 import { SessionProvider } from "./context/SessionContext";
 import { ProtectedRoute } from "./context/ProtectedRoutes";
+import {Register} from "./pages/Register";
 
 createRoot(document.getElementById("root")).render(
   <SessionProvider>
